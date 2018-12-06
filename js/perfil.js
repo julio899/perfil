b243fdf305e335f50c8fc9f9d87061aff63d93bc
Julio Vinachi <julio899@gmail.com>
2018-12-05 20:39:39 -0400

    Notes

--- js/perfil.js
+++ js/perfil.js
@@ -2,3 +2,6 @@
 particlesJS.load('particulas', 'js/particlesjs-config.json', function() {
   console.log('config loaded');
 });
+
+// ok  particles running 
+// now i need implement  sw / fetch / asyc awey / SPA / PWA
