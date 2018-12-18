39ad2e45e5789a34dee5bab929fc34aff021efc8
Julio Vinachi <julio899@gmail.com>
2018-12-17 23:45:48 -0400

    Update perfil.js

--- js/perfil.js
+++ js/perfil.js
@@ -5,3 +5,5 @@ particlesJS.load('particulas', 'js/particlesjs-config.json', function() {
 
 // ok  particles running 
 // now i need implement  sw / fetch / asyc awey / SPA / PWA / socket.io / and cors domain
+// check more dependency
+// the list the control
