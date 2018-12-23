1ecfb98f02d3dcdd50d64b6165d113236712c63b
Julio Vinachi <julio899@gmail.com>
2018-12-22 23:05:16 -0400

    Update perfil.js

--- js/perfil.js
+++ js/perfil.js
@@ -9,3 +9,8 @@ particlesJS.load('particulas', 'js/particlesjs-config.json', function() {
 // the list the control
 // sound display 1 warning in console and no is runing check in delay
 // ------------ ------------ ------------ ------------ ------------ ------------ ------------
+// https://www.browserling.com/tools/text-to-html-entities
+/*
+bitbucket.org:julio899/ap-servicio-externo.git
+   374d4c9..6b374c3  master -> master
+*/
