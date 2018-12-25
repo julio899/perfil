38db012877e8ee43ed16dd72672e23054ee6ea1a
Julio Vinachi <julio899@gmail.com>
2018-12-25 19:32:09 -0400

    Update perfil.js

--- js/perfil.js
+++ js/perfil.js
@@ -8,6 +8,7 @@ particlesJS.load('particulas', 'js/particlesjs-config.json', function() {
 // check more dependency
 // the list the control
 // sound display 1 warning in console and no is runing check in delay
+// AngularJs in webClaro
 // ------------ ------------ ------------ ------------ ------------ ------------ ------------
 // https://www.browserling.com/tools/text-to-html-entities
 /*
